9879efbf4d295f5d21425d3759c345a1ab85f52c
Jose Meza Llamosas <mezallamosas.j@northeastern.edu>
2025-03-11 13:54:46 -0400

    Moving functions to neurallm_utils.py

--- neurallm_utils.py
+++ neurallm_utils.py
@@ -173,3 +173,246 @@ def create_embedder(raw_embeddings: Word2Vec) -> torch.nn.Embedding:
     embedding.index_to_token = index_to_token
     #return embedding
     return embedding
+
+# -------------------------------
+# Data processing functions
+# -------------------------------
+
+def encode_tokens(data: list[list[str]], embedder: torch.nn.Embedding) -> list[list[int]]:
+    """
+    Replaces each natural-language token with its embedder index.
+
+    e.g. [["<s>", "once", "upon", "a", "time"],
+          ["there", "was", "a", ]]
+        ->
+        [[0, 59, 203, 1, 126],
+         [26, 15, 1]]
+        (The indices are arbitrary, as they are dependent on your embedder)
+
+    Params:
+        data: The corpus
+        embedder: An embedder trained on the given data.
+    """
+
+    finalList = []
+    for list in data:
+        currList = []
+        for word in list:
+            index = embedder.token_to_index[word]
+            currList.append(index)
+        finalList.append(currList)
+
+    return finalList
+
+
+def create_ngrams(tokens: list, n: int) -> list:
+    """Creates n-grams for the given token sequence.
+    Args:
+      tokens (list): a list of tokens as strings
+      n (int): the length of n-grams to create
+
+    Returns:
+      list: list of tuples of strings, each tuple being one of the individual n-grams
+    """
+    # STUDENTS IMPLEMENT
+    res = []
+    for i in range(0, len(tokens)-n):
+        #append n gram + yth value
+        res.append(tokens[i:i+n+1])
+    return res
+
+def generate_ngram_training_samples(encoded: list[list[int]], ngram: int) -> list:
+    """
+    Takes the **encoded** data (list of lists of ints) and 
+    generates the training samples out of it.
+    
+    Parameters:
+        up to you, we've put in what we used
+        but you can add/remove as needed
+    return: 
+    list of lists in the format [[x1, x2, ... , x(n-1), y], ...]
+    """
+
+    #1 2 3 4
+    #[1,2, y=3]
+    #[2,3, y=4]
+
+    # if you'd like to use tqdm, you can use it like this:
+    # for i in tqdm(range(len(encoded))):
+    final_list = []
+    for list in encoded:
+        currList = create_ngrams(list, ngram-1)
+        final_list.extend(currList)
+    return final_list
+
+def split_sequences(training_sample):
+    x_sample = []
+    y_sample = []
+    for line in training_sample:
+        x_sample.append(line[0:-1])
+        y_sample.append(line[-1])
+    return x_sample, y_sample
+
+def create_dataloaders(X: list, y: list, num_sequences_per_batch: int, 
+                       test_pct: float = 0.1, shuffle: bool = True) -> tuple[torch.utils.data.DataLoader]:
+    """
+    Convert our data into a PyTorch DataLoader.    
+    A DataLoader is an object that splits the dataset into batches for training.
+    PyTorch docs: 
+        https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+        https://pytorch.org/docs/stable/data.html
+
+    Note that you have to first convert your data into a PyTorch DataSet.
+    You DO NOT have to implement this yourself, instead you should use a TensorDataset.
+
+    You are in charge of splitting the data into train and test sets based on the given
+    test_pct. There are several functions you can use to acheive this!
+
+    The shuffle parameter refers to shuffling the data *in the loader* (look at the docs),
+    not whether or not to shuffle the data before splitting it into train and test sets.
+    (don't shuffle before splitting)
+
+    Params:
+        X: A list of input sequences
+        Y: A list of labels
+        num_sequences_per_batch: Batch size
+        test_pct: The proportion of samples to use in the test set.
+        shuffle: INSTRUCTORS ONLY
+
+    Returns:
+        One DataLoader for training, and one for testing.
+    """
+    
+    dataSet = TensorDataset(torch.tensor(X), torch.tensor(y))
+    test_size = int(len(dataSet)*test_pct)
+    train_size = len(dataSet) - test_size
+    train_data, test_data = torch.utils.data.random_split(dataSet, [train_size, test_size])
+    dataloader_train = DataLoader(train_data, batch_size=num_sequences_per_batch, shuffle=shuffle)
+    dataloader_test = DataLoader(test_data, batch_size=num_sequences_per_batch, shuffle=shuffle)
+    return dataloader_train, dataloader_test
+
+# -------------------------------
+# FFNN Model and Training Functions
+# -------------------------------
+
+class FFNN(nn.Module):
+    """
+    A Feed-Forward Neural Network for language modeling.
+    """
+    def __init__(self, vocab_size: int, ngram: int, embedding_layer: torch.nn.Embedding, hidden_units=128):
+        """
+        Initialize a new untrained model.
+        
+        Params:
+            vocab_size: Number of words in the vocabulary.
+            ngram: The N value (window size) for training.
+            embedding_layer: Pre-trained embedding layer.
+            hidden_units: Number of hidden units in the hidden layer.
+        """
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.ngram = ngram
+        self.embedding_layer = embedding_layer
+        self.hidden_units = hidden_units
+        
+        # Get embedding dimension from the provided embedder.
+        embedding_size = embedding_layer.embedding_dim
+        
+        # Define the network: flatten embedded n-gram tokens, then two linear layers with ReLU.
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(in_features=(ngram-1) * embedding_size, out_features=hidden_units, bias=True),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=vocab_size, bias=True)
+        )
+        
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass through the network.
+        
+        Params:
+            X: Tensor of input indices with shape (batch_size, ngram-1)
+        
+        Returns:
+            Logits of shape (batch_size, vocab_size).
+        """
+        embedded = self.embedding_layer(X)
+        flat_embedded = self.flatten(embedded)
+        logits = self.linear_relu_stack(flat_embedded)
+        return logits
+
+def train_one_epoch(dataloader, model, optimizer, loss_fn):
+    epoch_loss = 0
+    for inputs, labels in dataloader:
+        optimizer.zero_grad()                  # Zero gradients for this batch.
+        outputs = model(inputs)                # Forward pass.
+        batch_loss = loss_fn(outputs, labels)  # Compute loss.
+        batch_loss.backward()                  # Backpropagation.
+        optimizer.step()                       # Update weights.
+        epoch_loss += batch_loss.item()
+    return epoch_loss
+
+def train(dataloader, model, epochs: int = 1, lr: float = 0.001) -> None:
+    """
+    Train the model.
+    
+    Params:
+        dataloader: Training data loader.
+        model: The model to train.
+        epochs: Number of epochs.
+        lr: Learning rate.
+    """
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    loss_fn = nn.CrossEntropyLoss()
+    n_batches = len(dataloader)
+    
+    model.train()  # Set the model to training mode.
+    
+    for epoch in tqdm(range(epochs), desc="Training Epochs"):
+        epoch_loss = train_one_epoch(dataloader, model, optimizer, loss_fn)
+        avg_epoch_loss = epoch_loss / n_batches
+        print(f"Epoch: {epoch}, Average Loss: {avg_epoch_loss:.4f}")
+        # Log metrics to wandb
+        wandb.log({"epoch": epoch, "avg_epoch_loss": avg_epoch_loss})
+
+def full_pipeline(data, word_embeddings_filename: str, 
+                  batch_size: int,
+                  ngram: int,
+                  hidden_units: int = 128,
+                  epochs: int = 1,
+                  lr: float = 0.001,
+                  test_pct: float = 0.1) -> FFNN:
+    """
+    Run the full training pipeline from loading embeddings to model training.
+    
+    Params:
+        data: Raw data as a list of lists of tokens (here, integer indices).
+        word_embeddings_filename: Filename for the pre-trained embeddings.
+        batch_size: Batch size for training.
+        ngram: N-gram size.
+        hidden_units: Number of hidden units.
+        epochs: Number of epochs.
+        lr: Learning rate.
+        test_pct: Percentage of data for testing (not used in training).
+    
+    Returns:
+        The trained FFNN model.
+    """
+    # Load embeddings and create an embedder.
+    token_embeddings = load_word2vec(word_embeddings_filename)
+    embedder = create_embedder(token_embeddings)
+    
+    # Preprocess data.
+    encoded_tokens = encode_tokens(data, embedder)
+    vocab_size = embedder.num_embeddings
+    training_sample = generate_ngram_training_samples(encoded_tokens, ngram)
+    x_sample, y_sample = split_sequences(training_sample)
+    dataloader_train, _ = create_dataloaders(x_sample, y_sample, batch_size, test_pct)
+    
+    # Initialize the model.
+    model = FFNN(vocab_size=vocab_size, ngram=ngram, embedding_layer=embedder, hidden_units=hidden_units)
+    
+    # Train the model.
+    train(dataloader=dataloader_train, model=model, epochs=epochs, lr=lr)
+    
+    return model
